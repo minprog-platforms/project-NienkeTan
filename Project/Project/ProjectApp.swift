83b0436070da01f96c5bda7a93dcee309d83ccb2
Nienke Tan <ntan@live.nl>
2022-04-29 14:04:20 +0200

    first commit

---
+++ Project/Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Nienke Tan on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
