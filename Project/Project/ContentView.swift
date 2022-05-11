b205da522d42468fb8402e96191377702b29db15
Nienke Tan <ntan@live.nl>
2022-05-11 09:03:27 +0200

    nog een poging

--- Project/Project/ContentView.swift
+++ Project/Project/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
 
-    let weekdays : [String] = ["M","T", "W", "T", "F", "S", "S"]
     init(){
         UINavigationBar.appearance()
             .largeTitleTextAttributes = [.foregroundColor: UIColor.white]
@@ -19,34 +18,9 @@ struct ContentView: View {
         TabView {
             NavigationView {
                 ZStack{
-
                     BackgroundMain()
-
                     VStack{
-                        
-                        HStack(){
-                            ForEach(weekdays, id: \.self) {
-                                weekDay in
-                                VStack{
-                                    Button{
-                                        
-                                    }label: {Text(weekDay)
-                                            .foregroundColor(Color("textColorlight"))
-                                            .font(.system(size: 20, weight: .medium))
-                                            .frame(width: 50, height: 50)
-                                            .background(.white)
-                                        
-                                    }
-                                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-                                    
-                                    Circle()
-                                        .stroke(Color("backgroundColorblue"), lineWidth:  1.5)
-                                        .frame(width: 15, height: 15)
-                                        .foregroundColor(Color("textColorlight"))
-                                    
-                                }
-                            }
-                        }.padding(.vertical, 15)
+                        WeekdaysBar()
                         
                         List(0..<3) { item in
                             HStack{
@@ -59,16 +33,13 @@ struct ContentView: View {
                         }
                         
                         Spacer()
-                        
                     }
-                    
                 }.navigationTitle("Dinner Planner")
                 
             }.tabItem {
                 Image(systemName: "calendar").foregroundColor(.black)
                     .font(.system(size: CGFloat(40)))
                     .frame(width: 120, height: 40.0)
-                
                 
                 Text("Calendar").foregroundColor(.black)
                     .font(.system(size: 20, weight: .light))
---
+++ Project/WeekdaysBar.swift
@@ -0,0 +1,36 @@
+//
+//  WeekdaysBar.swift
+//  Project
+//
+//  Created by Nienke Tan on 11/05/2022.
+//
+
+import SwiftUI
+
+struct WeekdaysBar: View {
+    var body: some View {
+        HStack(){
+            ForEach(["M","T", "W", "T", "F", "S", "S"], id: \.self) {
+                weekDay in
+                VStack{
+                    Button{
+                        
+                    }label: {Text(weekDay)
+                            .foregroundColor(Color("textColorlight"))
+                            .font(.system(size: 20, weight: .medium))
+                            .frame(width: 50, height: 50)
+                            .background(.white)
+                        
+                    }
+                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                    
+                    Circle()
+                        .stroke(Color("backgroundColorblue"), lineWidth:  1.5)
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(Color("textColorlight"))
+                    
+                }
+            }
+        }.padding(.vertical, 15)
+    }
+}
